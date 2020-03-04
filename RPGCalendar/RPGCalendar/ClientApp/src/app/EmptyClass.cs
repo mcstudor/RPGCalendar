b5663f3c640cfcb663d78d43c0cc5c8d86e7e73f
Gustavo <Leyva>
2020-03-04 12:50:42 -0800

    Log in dummy
    added log in dummy to the home component page, messed a bit with the .css to add colors.

---
+++ RPGCalendar/RPGCalendar/ClientApp/src/app/EmptyClass.cs
@@ -0,0 +1,10 @@
+using System;
+namespace RPGCalendar.ClientApp.src.app
+{
+  public class EmptyClass
+  {
+    public EmptyClass()
+    {
+    }
+  }
+}
---
+++ RPGCalendar/RPGCalendar/ClientApp/src/app/component-help/component-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-help',
+  templateUrl: './component-help.component.html',
+  styleUrls: ['./component-help.component.css']
+})
+export class ComponentHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
