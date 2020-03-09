454621507821b24824a00488e36d13c74bdc9b36
Emilio Campos <45043427+ecampos3@users.noreply.github.com>
2020-03-08 20:46:02 -0700

    Added RPGCalendar.data

---
+++ RPGCalendar/RPGCalendar.Data/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPGCalendar.Data
+{
+    public class EntityBase
+    {
+        [Required]
+        public int id { get; protected set; }
+    }
+}
---
+++ RPGCalendar/RPGCalendar.Data/FingerPrintEntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPGCalendar.Data
+{
+    public class FingerPrintEntityBase : EntityBase
+    {
+        [Required]
+        public string? CreatedBy { get; internal set; }
+        [Required]
+        public DateTime CreatedOn { get; internal set; }
+        [Required]
+        public string? ModifiedBy { get; internal set; }
+        [Required]
+        public DateTime ModifiedOn { get; internal set; }
+    }
+}
