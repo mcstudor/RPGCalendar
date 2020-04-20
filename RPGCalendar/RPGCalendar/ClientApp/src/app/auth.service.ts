0ac5f077c71e247a9c5d2b9d920d03dfad7bbf50
Gustavo <Leyva>
2020-04-20 11:24:42 -0700

    added layout changes to login, registration, password reset link, forgot-password view,

--- RPGCalendar/RPGCalendar/ClientApp/src/app/auth.service.ts
+++ RPGCalendar/RPGCalendar/ClientApp/src/app/auth.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const BASEURL = 'http://localhost:4200';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public login(userInfo: User){
     localStorage.setItem('ACCESS_TOKEN', "access_token");
   }
+  requestReset(body): Observable<any> {
+    return this.http.post(`${BASEURL}/req-reset-password`, body);
+  }
+
+  registerUser(body): Observable<any> {
+    return this.http.post(`${BASEURL}/register`, body);
+  }
+
+  newPassword(body): Observable<any> {
+    return this.http.post(`${BASEURL}/new-password`, body);
+  }
+
+  ValidPasswordToken(body): Observable<any> {
+    return this.http.post(`${BASEURL}/valid-password-token`, body);
+  }
+
+  loginUser(body): Observable<any> {
+    return this.http.post(`${BASEURL}/login`, body);
+  }
+
 
   public isLoggedIn(){
     return localStorage.getItem('ACCESS_TOKEN') !== null;
--- RPGCalendar/RPGCalendar/ClientApp/src/app/sign-up/signup.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './signup.component.html',
-})
-export class SignUpComponent {
-}
